dcad633e50af52c833aac671658d87ec4bb3f46b
huuga <huuga@inbox.ru>
2021-04-06 20:55:21 +0300

    ft_memmove.c

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,22 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/06 01:06:36 by marvin            #+#    #+#             */
-/*   Updated: 2021/04/06 01:06:36 by marvin           ###   ########.fr       */
+/*   Created: 2021/04/06 20:22:58 by marvin            #+#    #+#             */
+/*   Updated: 2021/04/06 20:22:58 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 
-size_t		ft_strlen(char *str);
-void		*ft_memset(void *des, int c, size_t n);
-void		ft_bzero(void *des, size_t n);
-void		*ft_memcpy(void *dest, const void *src, size_t n);
-void		*ft_memccpy(void *dest, const void *src, int c, size_t n);
-void		*ft_memmove(void *dest, const void *src, size_t n);
+#include "libft.h"
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+	char	*buffer;
+	char	*buffer_copy;
+	size_t	n_copy;
+
+	n_copy = n;
+	buffer = (char *)malloc(sizeof(char) * n);
+	buffer_copy = buffer;
+	while(n > 0)
+	{
+		*buffer = *(char *)src;
+		buffer++;
+		src++;
+		n--;
+	}
+	ft_memcpy(dest, buffer_copy, n_copy);
+	free(buffer_copy);
+	return (dest);
+}
--- libft/main.c
+++ libft/main.c
@@ -17,7 +17,7 @@ int		main()
 
 
 	mem = malloc(1);
-	char	*mem2 = malloc(1);
+	//char	*mem2 = malloc(1);
 
 	// ft_bzero(s, 3);
 	// printf("%c", s[0]);
@@ -27,17 +27,17 @@ int		main()
 	// printf("%c", s[4]);
 	// printf("%c\n", s[5]);
 
-	char *yo = ft_memcpy(mem, s, 60);
+	ft_memmove(s, s + 3, 2);
 
-	printf("look here %s\n", yo);
+	printf("look here %s\n", s);
 
-	char *yoyo = ft_memccpy(mem2, s, 'r', 9);
-	if (yoyo == NULL)
-	{
-		write (1, "yoyo is null\n", 13);
-		return (1);
-	}
-	printf("%s\n", yoyo);
+	// char *yoyo = ft_memccpy(mem2, s, 'r', 9);
+	// if (yoyo == NULL)
+	// {
+	// 	write (1, "yoyo is null\n", 13);
+	// 	return (1);
+	// }
+	// printf("%s\n", yoyo);
 	free(mem);
 
 }
